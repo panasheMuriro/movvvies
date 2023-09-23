f939b65f02ca76fb72522f9c6ebbad968e96378c
murirop <panashemuriro2023@u.northwestern.edu>
2023-09-22 20:03:16 -0500

    remove people models and add movie models

--- movvvies/DetailsViewController.h
+++ movvvies/DetailsViewController.h
@@ -7,15 +7,24 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import "Person.h"
+#import "MovieModel.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface DetailsViewController : UIViewController
-@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
-@property (weak, nonatomic) IBOutlet UILabel *ageLabel;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UITextView *captionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *genreLabel;
+@property (weak, nonatomic) IBOutlet UILabel *ratingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *yearLabel;
 
-@property (nonatomic, strong) Person *selectedPerson;
+@property (weak, nonatomic) IBOutlet UIImageView *coverPhoto;
+
+
+
+// picture
+
+@property (nonatomic, strong) MovieModel *selectedMovie;
 
 @end
 
--- movvvies/DetailsViewController.m
+++ movvvies/DetailsViewController.m
@@ -13,8 +13,48 @@
     [super viewDidLoad];
     
     // Assuming you have a "selectedPerson" property containing the person's details
-    self.nameLabel.text = self.selectedPerson.name;
-    self.ageLabel.text =[NSString stringWithFormat:@"Age: %@", self.selectedPerson.age];
+//    self.nameLabel.text = self.selectedPerson.name;
+//    self.ageLabel.text =[NSString stringWithFormat:@"Age: %@", self.selectedPerson.age];
+    
+    self.titleLabel.text = self.selectedMovie.title;
+    self.captionLabel.text = self.selectedMovie.caption;
+    self.genreLabel.text =[NSString stringWithFormat:@"Genre: %@", self.selectedMovie.genre];
+    self.yearLabel.text =[NSString stringWithFormat:@"Year: %@", self.selectedMovie.year];
+    self.ratingLabel.text =[NSString stringWithFormat:@"Rating: %@", self.selectedMovie.rating];
+    
+    
+    // Define the URL of the image you want to display
+    NSString *imageUrlString =  self.selectedMovie.coverPhotoLink;
+    NSURL *imageUrl = [NSURL URLWithString:imageUrlString];
+
+    // Create an NSURLSessionDataTask to fetch the image data
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithURL:imageUrl completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"Error fetching image: %@", error);
+            return;
+        }
+        
+        // Check if the response is an HTTP response and the status code is OK
+        if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
+            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+            if (httpResponse.statusCode == 200) {
+                // Create an image from the data
+                UIImage *image = [UIImage imageWithData:data];
+                
+                // Update the UI on the main thread
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    // Set the fetched image to your UIImageView
+                    self.coverPhoto.image = image;
+                });
+            } else {
+                NSLog(@"HTTP status code: %ld", (long)httpResponse.statusCode);
+            }
+        }
+    }];
+    
+    [task resume];
+    
+    
 }
 
 @end
